b214c9e370fbbafbc5c33739c1a136969bac7271
sshah645 <sshah645@B107-023.ad.mtroyal.ca>
2020-01-15 16:24:24 -0700

    Sadman Shahryier isPrime commit (with break)

--- src/test2/HelloWorld.java
+++ src/test2/HelloWorld.java
@@ -2,8 +2,31 @@ package test2;
 
 public class HelloWorld {
 	public static void main (String[] args) {
-		System.out.println("Hello, world.");
-		System.out.println("Commit by Munima ");
+		System.out.println(isPrime(2));
+	    System.out.println(isPrime(3));
+	    System.out.println(isPrime(4));
+	    System.out.println(isPrime(10));
+	    System.out.println(isPrime(11));
+	    System.out.println(isPrime(12));
 	}
+	
+	public static boolean isPrime(int num){
+	    boolean isPrime = true;
+	    //if isDivisible is true, the num is deemed as a composite number i.e not a prime
+	    for (int i = 2; i < num; i++){
+	      if (isDivisible(num, i)){
+	        isPrime = false;
+	        break;
+	      }
+	    }
+	    
+	    return isPrime;
+	    }
+	
+	public static boolean isDivisible(int dividend, int divisor){
+	    if (dividend % divisor == 0){
+	      return true;
+	    } else return false;
+	  }
 
 }
